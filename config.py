84aea0f93a6483f252b6ebe8583ddddce6ff0d32
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2024-11-26 11:41:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ load_dotenv()
 
 class Config(object):
     # Get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6421258723:AAF8AuYEYAQy_1OeAoXDcEB8B1ujlu5QqjQ")
     # The Telegram API things
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = os.environ.get("API_ID", "22808125")
+    API_HASH = os.environ.get("API_HASH", "406c09543fec722fe3c48ca2f06d78de")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
 
@@ -35,7 +35,7 @@ class Config(object):
     # Set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3700
 
-    OWNER_ID = os.environ.get("6865155711")
+    OWNER_ID = os.environ.get("819861991")
     ADL_BOT_RQ = {}
-    AUTH_USERS = list({int(x) for x in os.environ.get("AUTH_USERS", "0").split()})
+    AUTH_USERS = list({int(x) for x in os.environ.get("AUTH_USERS", "819861991").split()})
     AUTH_USERS.append(OWNER_ID)
